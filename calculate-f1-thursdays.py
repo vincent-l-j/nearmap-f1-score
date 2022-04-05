9b81dd5082e1faf6db2364276374b52bc6572aa9
Vincent Jadraque <vincent.jadraque@gmail.com>
2022-04-05 14:16:08 +1000

    Refactor f1 score calculation into a function to parameterise the day of the week

--- calculate-f1-thursdays.py
+++ calculate-f1-thursdays.py
@@ -12,11 +12,33 @@ def main():
         parse_dates=True,
         index_col='dates',
     )
-    # filter out thursdays
-    df_thursdays = df[df.index.weekday == 3]
-    # calculate f1 score
-    f1_score = metrics.f1_score(df_thursdays['y'], df_thursdays['yhat'])
+    # Thursday
+    day = 3
+    # calculate f1 score for Thursdays
+    f1_score = calculate_weekday_f1_score(df, day)
     print(f"{f1_score:.5f}")
+
+
+def calculate_weekday_f1_score(df, day):
+    """
+    Calculate the f1 score for a particular day of the week.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The dataframe with test and prediction values. It is
+        assumed that the test column is named `y` and prediction
+        column is named `yhat`.
+    day : int
+        The day of the week. It is assumed the week starts on Monday,
+        which is denoted by 0 and ends on Sunday which is denoted by 6.
+    """
+    # filter out day of the week
+    df_weekday = df[df.index.weekday == day]
+    # calculate f1 score
+    f1_score = metrics.f1_score(df_weekday['y'], df_weekday['yhat'])
+
+    return f1_score
 
 
 if __name__ == "__main__":
